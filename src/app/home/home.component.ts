c479995ab726927269c64245363290d390aa1e2a
Patrick Bauersfeld <patrick.bauersfeld@gmx.ch>
2021-06-01 11:34:55 +0200

    add facivon.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable, timer} from "rxjs";
+import {map, shareReplay} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
@@ -6,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  onlyClock = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  private currentTime: Observable<Date> = timer(0, 1000).pipe(
+    map(tick => new Date()),
+    shareReplay(1)
+  );
+
+  get time() {
+    return this.currentTime;
+  }
+
+  clickOnClock(): void {
+    this.onlyClock = !this.onlyClock;
   }
 }
